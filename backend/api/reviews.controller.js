e50d4b27e3d37b164563bbe4845b33e712ee4765
daliovic <dalidaliovic@gmail.com>
2022-12-08 16:48:15 +0300

    First Commit

---
+++ backend/api/reviews.controller.js
@@ -0,0 +1,16 @@
+import RestaurantsDAO from '../dao/restaurantsDAO.js'
+
+export default class ReviewsController {
+  static async apiGetReview(req, res, next) {
+    res.json({})
+  }
+  static async apiPostReview(req, res, next) {
+    res.json({})
+  }
+  static async apiUpdateReview(req, res, next) {
+    res.json({})
+  }
+  static async apiDeleteReview(req, res, next) {
+    res.json({})
+  }
+}
